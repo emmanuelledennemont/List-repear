ea3447867c2cda0b02995a8a0094f0224125b6cf
emmanuelle <emmanuelledennemont@orange.fr>
2024-06-19 18:22:54 +0400

    Initial Commit

---
+++ Dashbord/DashbordApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashbordApp.swift
+//  Dashbord
+//
+//  Created by Emmanuelle  Dennemont on 19/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct DashbordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
